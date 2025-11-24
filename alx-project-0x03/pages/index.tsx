94bf7119b158dcb299e259209f990fd3e5715bf2
Theophilus Frimpong <theophilusfrimpong17@gmail.com>
2025-11-24 02:19:20 +0000

    Refactor Home component for improved code readability; standardize formatting and add semicolons for consistency

--- alx-project-0x03/pages/index.tsx
+++ alx-project-0x03/pages/index.tsx
@@ -3,12 +3,12 @@ import { useRouter } from "next/router";
 import { PageRouteProps } from "@/interface";
 
 export default function Home() {
-  const router = useRouter()
+  const router = useRouter();
 
   // Imeperative routing with useRouter
-  const routeToNextPage  = ({ pageRoute }: PageRouteProps) => {
-    router.push(pageRoute, undefined, { shallow: false})
-  }
+  const routeToNextPage = ({ pageRoute }: PageRouteProps) => {
+    router.push(pageRoute, undefined, { shallow: false });
+  };
 
   return (
     <div className="min-h-screen bg-gray-100 flex flex-col justify-center items-center text-center">
@@ -23,9 +23,21 @@ export default function Home() {
 
       {/* Navigation Options */}
       <div className="flex gap-6">
-        <Button action={() => routeToNextPage({ pageRoute: '/generate-text-ai' })} buttonLabel="Generate Text" buttonBackgroundColor="blue" />
-        <Button action={() => routeToNextPage({ pageRoute: '/text-to-image'})} buttonLabel="Text to Image" buttonBackgroundColor="green" />
-        <Button action={() => routeToNextPage({ pageRoute: '/counter-app'})} buttonLabel="Contact us" buttonBackgroundColor="orange" />
+        <Button
+          action={() => routeToNextPage({ pageRoute: "/generate-text-ai" })}
+          buttonLabel="Generate Text"
+          buttonBackgroundColor="blue"
+        />
+        <Button
+          action={() => routeToNextPage({ pageRoute: "/text-to-image" })}
+          buttonLabel="Text to Image"
+          buttonBackgroundColor="green"
+        />
+        <Button
+          action={() => routeToNextPage({ pageRoute: "/counter-app" })}
+          buttonLabel="Contact us"
+          buttonBackgroundColor="orange"
+        />
       </div>
     </div>
   );
